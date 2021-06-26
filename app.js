7044183aa0a466d65d7ddbd376677243ef8fbdb5
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-06-25 19:55:49 -0700

    Validated answers, added option to add github link

--- app.js
+++ app.js
@@ -14,12 +14,28 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name?',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username:'
+      message: 'Enter your GitHub Username (Required):',
+      validate: githubnameInput => {
+        if (githubnameInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub username!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -40,18 +56,47 @@ const promptProject = portfolioData => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?'
+      message: 'What is the name of your project? (Required)',
+      validate: projectName => {
+        if (projectName) {
+          return true;
+        } else {
+          console.log('Please enter the name of your project!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: projectDescription => {
+        if (projectDescription) {
+          return true;
+        } else {
+          console.log('Please give a description of the project!');
+          return false;
+        }
+      }
     },
     {
       type: 'checkbox',
       name: 'languages',
       message: 'What did you build this project with? (Check all that apply)',
       choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: githubLink => {
+        if (githubLink) {
+          return true;
+        } else {
+          console.log('Please enter the GitHub link to your project.');
+          return false;
+        }
+      }
     },
     {
       type: 'confirm',
